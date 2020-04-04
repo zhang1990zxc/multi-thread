2264e0b151fb6789dc82a4509c6558b21bb82f70
zhang <1018736264@qq.com>
2020-04-04 21:50:17 +0800

    commit

---
+++ src/main/java/com/zhang/thread/chapter2/synchronizedMethodLockObject/MyObject.java
@@ -0,0 +1,11 @@
+package com.zhang.thread.chapter2.synchronizedMethodLockObject;
+
+/**
+ * @ClassName MyObject
+ * @Description 整条街最靓的仔，写点注释吧
+ * @Author zhang
+ * @Date 2020/4/4 21:49
+ * @Version 1.0
+ **/
+public class MyObject {
+}
